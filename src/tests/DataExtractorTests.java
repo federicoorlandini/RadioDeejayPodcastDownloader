26ffd7566a1c9a55c380f141c8460fa8088d0ec5
federico.orlandini@cxcompany.com <federico.orlandini@cxcompany.com>
2022-05-31 10:06:39 +0200

    Adding unit tests

---
+++ src/tests/DataExtractorTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class DataExtractorTests {
+    @Test
+    public void Test1()
+    {
+        Assert.assertEquals(true, false);
+    }
+}
